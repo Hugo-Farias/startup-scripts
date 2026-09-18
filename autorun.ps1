2621ece758af7bfca9d39e2f51a11f60e1f3b7a0
Hugo-Farias <hg80rod@gmail.com>
2026-09-18 10:23:38 -0300

    add: upload function for ease of mantaining

--- autorun.ps1
+++ autorun.ps1
@@ -2,7 +2,7 @@ $drive = "gdrive:rclone"
 $documents = "D:/Users/Hugo/Documents"
 $options = @(
     "-v"
-    "-P"
+    # "-P"
     # "--copy-links"
     "--exclude", "**/.git/**"
     "--exclude", "**/*.log"
@@ -10,27 +10,40 @@ $options = @(
     # "--dry-run"
 )
 
-rclone copy "D:/OneDrive/" "$drive/onedrive" $options
-rclone copy "$HOME/.config" "$drive/.config" --max-size 10M $options
-rclone copy "$documents/startup-scripts" "$drive/startup-scripts" $options
-rclone copy "C:/Program Files/EqualizerAPO/config/config.txt" "$drive/equalizerapo/config" $options
-rclone copy "$documents/Curriculo/" "$drive/curriculo" $options
-rclone copy "$documents/Scripts/" "$drive/scripts" $options
-rclone copy "$documents/AutoHotKey/" "$drive/autohotkey" $options
-rclone copy "$HOME/AppData/Roaming/steam-rom-manager/userData/" "$drive/steam-rom-manager/userdata" $options
-rclone copy "C:/Aplications/Kanata/holdMotion.kbd" "$drive/kanata" $options
-rclone copy "$documents/System_Documents/KoeiTecmo/" "$drive/saves/koeitecmo" $options
-rclone copy "D:/Games/!Emulation/3DS/#/.config/sdmc" "$drive/saves/3ds/sdmc" $options
-rclone copy "$documents/System_Documents/Xenia" "$drive/saves/xenia/" $options
-rclone copy "$documents/System_Documents/PowerShell" "$drive/powershell/" $options
-rclone copy "$HOME/AppData/Local/TekkenGame/Saved/SaveGames/TEKKEN7" "$drive/saves/tekken7" $options
+function upload {
+    param (
+        [string]$Name,
+        [string]$Source,
+        [string]$Destination,
+        [string[]]$AdditionalOptions = @()
+    )
 
-rclone copy "C:/Users/Hugo/AppData/Roaming/Mailspring/config.json" "$drive/mailspring" $options
-rclone copy "$HOME/AppData/Roaming/Mailspring/keymap.json" "$drive/mailspring" $options
+    Write-Host "Uploading $Name..."
+    rclone copy $Source $Destination $options $AdditionalOptions
+}
 
-rclone copy "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/History-journal" "$drive/browser/chromium/history" $options
-rclone copy "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/History" "$drive/browser/chromium/history" $options
-rclone copy "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/Bookmarks" "$drive/browser/chromium/bookmarks" $options
-rclone copy "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/BookmarkMergedSurfaceOrdering" "$drive/browser/chromium/bookmarks" $options
+upload ".config" "$HOME/.config" "$drive/.config" @("--max-size", "10M")
+upload "startup-scripts" "$documents/startup-scripts" "$drive/startup-scripts"
+upload "EqualizerAPO config" "C:/Program Files/EqualizerAPO/config/config.txt" "$drive/equalizerapo/config"
+upload "Curriculo" "$documents/Curriculo/" "$drive/curriculo"
+upload "Scripts" "$documents/Scripts/" "$drive/scripts"
+upload "AutoHotKey" "$documents/AutoHotKey/" "$drive/autohotkey"
+upload "steam-rom-manager" "$HOME/AppData/Roaming/steam-rom-manager/userData/" "$drive/steam-rom-manager/userdata"
+upload "Kanata" "C:/Aplications/Kanata/holdMotion.kbd" "$drive/kanata"
+upload "Saves:KoeiTecmo" "$documents/System_Documents/KoeiTecmo/" "$drive/saves/koeitecmo"
+upload "Saves:3DS" "D:/Games/!Emulation/3DS/#/.config/sdmc" "$drive/saves/3ds/sdmc"
+upload "Saves:Xenia" "$documents/System_Documents/Xenia" "$drive/saves/xenia/"
+upload "Saves:Tekken 7" "$HOME/AppData/Local/TekkenGame/Saved/SaveGames/TEKKEN7" "$drive/saves/tekken7"
+upload "PowerShell config" "$documents/System_Documents/PowerShell" "$drive/powershell/"
+
+upload "Mailspring config" "C:/Users/Hugo/AppData/Roaming/Mailspring/config.json" "$drive/mailspring"
+upload "Mailspring keymap" "$HOME/AppData/Roaming/Mailspring/keymap.json" "$drive/mailspring" $options
+
+upload "Browser history" "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/History" "$drive/browser/chromium/history" $options
+upload "Browser history-journal" "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/History-journal" "$drive/browser/chromium/history" $options
+upload "Browser Bookmarks" "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/Bookmarks" "$drive/browser/chromium/bookmarks" $options
+upload "Browser Bookmark Merged" "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/BookmarkMergedSurfaceOrdering" "$drive/browser/chromium/bookmarks" $options
+
+upload "Onedrive" "D:/OneDrive/" "$drive/onedrive" $options
 
 rclone tree "$drive" -all
