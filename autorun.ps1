ce0f40b024fc20f5ac393b578d3a50e92a7b5320
Hugo-Farias <hg80rod@gmail.com>
2026-09-18 05:30:15 -0300

    add: mailspring, brave browser history

--- autorun.ps1
+++ autorun.ps1
@@ -1,19 +1,34 @@
 $drive = "gdrive:rclone"
 $documents = "D:/Users/Hugo/Documents"
-$exclusions = "**/.git/**"
+$options = @(
+    "-v"
+    "-P"
+    # "--copy-links"
+    "--exclude", "**/.git/**"
+    "--exclude", "**/*.log"
+    "--exclude", "**/.log/**"
+    # "--dry-run"
+)
 
-rclone copy "$HOME/.config" "$drive/.config" -v -P --max-size 10M  --exclude $exclusions
-rclone copy "C:/Program Files/EqualizerAPO/config/config.txt" "$drive/equalizerapo/config" -v -P --exclude $exclusions
-rclone copy "$documents/startup-scripts" "$drive/startup-scripts" -v -P --exclude $exclusions
-rclone copy "$documents/Curriculo/" "$drive/curriculo" -v -P --exclude $exclusions
-rclone copy "$documents/Scripts/" "$drive/scripts" -v -P --exclude $exclusions
-rclone copy "$documents/AutoHotKey/" "$drive/autohotkey" -v -P --exclude $exclusions
-rclone copy "$HOME/AppData/Roaming/steam-rom-manager/userData/" "$drive/steam-rom-manager/userdata" -v -P --exclude $exclusions
-rclone copy "C:/Aplications/Kanata/holdMotion.kbd" "$drive/kanata" -v -P --exclude $exclusions
-rclone copy "D:/OneDrive/" "$drive/onedrive" -v -P --exclude $exclusions
-rclone copy "$documents/System_Documents/KoeiTecmo/" "$drive/saves/koeitecmo" -v -P --exclude $exclusions
-rclone copy "D:/Games/!Emulation/3DS/#/.config/sdmc" "$drive/saves/3ds/sdmc" -v -P --exclude $exclusions
-rclone copy "$documents/System_Documents/Xenia" "$drive/saves/xenia/" -v -P --exclude $exclusions
-rclone copy "$documents/System_Documents/PowerShell" "$drive/powershell/" -v -P --exclude $exclusions
-rclone copy "$HOME/AppData/Local/TekkenGame/Saved/SaveGames/TEKKEN7" "$drive/saves/tekken7" -v -P --exclude $exclusions
+# rclone copy "D:/OneDrive/" "$drive/onedrive" $options
+rclone copy "$HOME/.config" "$drive/.config" --max-size 10M $options
+rclone copy "$documents/startup-scripts" "$drive/startup-scripts" $options
+rclone copy "C:/Program Files/EqualizerAPO/config/config.txt" "$drive/equalizerapo/config" $options
+rclone copy "$documents/Curriculo/" "$drive/curriculo" $options
+rclone copy "$documents/Scripts/" "$drive/scripts" $options
+rclone copy "$documents/AutoHotKey/" "$drive/autohotkey" $options
+rclone copy "$HOME/AppData/Roaming/steam-rom-manager/userData/" "$drive/steam-rom-manager/userdata" $options
+rclone copy "C:/Aplications/Kanata/holdMotion.kbd" "$drive/kanata" $options
+rclone copy "$documents/System_Documents/KoeiTecmo/" "$drive/saves/koeitecmo" $options
+rclone copy "D:/Games/!Emulation/3DS/#/.config/sdmc" "$drive/saves/3ds/sdmc" $options
+rclone copy "$documents/System_Documents/Xenia" "$drive/saves/xenia/" $options
+rclone copy "$documents/System_Documents/PowerShell" "$drive/powershell/" $options
+rclone copy "$HOME/AppData/Local/TekkenGame/Saved/SaveGames/TEKKEN7" "$drive/saves/tekken7" $options
+
+rclone copy "C:/Users/Hugo/AppData/Roaming/Mailspring/config.json" "$drive/mailspring" $options
+rclone copy "$HOME/AppData/Roaming/Mailspring/keymap.json" "$drive/mailspring" $options
+
+rclone copy "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/History-journal" "$drive/browser/chromium/history" $options
+rclone copy "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/History" "$drive/browser/chromium/history" $options
+
 rclone tree "$drive"
