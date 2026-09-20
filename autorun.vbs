867c10d78d9f1382b141541b9c2df76258305c1b
Hugo-Farias <hg80rod@gmail.com>
2026-09-20 02:01:02 -0300

    add: log creation to rclone-backup

--- autorun.vbs
+++ autorun.vbs
@@ -1,6 +1,6 @@
 Function RunPowerShell(script)
     CreateObject("WScript.Shell").Run _
-        "powershell.exe -NoProfile -WindowStyle Hidden -File """ & script & """", 0, False
+        "powershell.exe -NoProfile -File """ & script & """", 0, False
 End Function
 
 RunPowerShell "D:/Users/Hugo/Documents/startup-scripts/rclone-backup.ps1"
--- rclone-backup.ps1
+++ rclone-backup.ps1
@@ -1,16 +1,27 @@
+$script_name = "rclone-backup"
 $Host.UI.RawUI.WindowTitle = "rclone-scheduled-backup"
+
+$date = Get-Date -Format 'yyyy.MM.dd-HH.mm.ss'
+$log = "$($script_name)_$($date).log"
+New-Item -ItemType Directory -Path "$PSScriptRoot/logs" -Force | Out-Null
+Start-Transcript -Path "$PSScriptRoot/logs/$log"
+
 $drive = "gdrive:rclone"
 $documents = "D:/Users/Hugo/Documents"
+$appdata = "C:/Users/Hugo/AppData"
 $options = @(
     "-v"
     "-P"
-    # "--copy-links"
     "--exclude", ".gitignore"
     "--exclude", ".git/**"
     "--exclude", "**/.gitignore"
     "--exclude", "**/.git*/**"
     "--exclude", "**/*.log"
+    "--exclude", ".log/**"
     "--exclude", "**/.log/**"
+    "--exclude", "**/logs/**"
+    "--exclude", "logs/**"
+    # "--copy-links"
     # "--dry-run"
 )
 
@@ -23,10 +34,11 @@ function upload {
     )
 
     Write-Host "Uploading $Name..."
+    Write-Host $date
     rclone copy $Source $Destination $options $AdditionalOptions
 }
 
-upload ".config" "$HOME/.config" "$drive/.config" @("--max-size", "10M", "--exclude", "nvim/**")
+upload ".config" "$HOME/.config" "$drive/.config" @("--max-size", "10M")
 upload "startup-scripts" "$documents/startup-scripts" "$drive/startup-scripts"
 upload "EqualizerAPO config" "C:/Program Files/EqualizerAPO/config/config.txt" "$drive/equalizerapo/config"
 upload "Curriculo" "$documents/Curriculo/" "$drive/curriculo"
@@ -39,12 +51,13 @@ upload "Saves:3DS" "D:/Games/!Emulation/3DS/#/.config/sdmc" "$drive/saves/3ds/sd
 upload "Saves:Xenia" "$documents/System_Documents/Xenia" "$drive/saves/xenia/"
 upload "Saves:Tekken 7" "$HOME/AppData/Local/TekkenGame/Saved/SaveGames/TEKKEN7" "$drive/saves/tekken7"
 upload "PowerShell config" "$documents/System_Documents/PowerShell" "$drive/powershell/"
-upload "Mailspring config" "C:/Users/Hugo/AppData/Roaming/Mailspring/config.json" "$drive/mailspring"
+upload "Mailspring config" "$appdata/Roaming/Mailspring/config.json" "$drive/mailspring"
 upload "Mailspring keymap" "$HOME/AppData/Roaming/Mailspring/keymap.json" "$drive/mailspring"
-upload "Browser history" "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/History" "$drive/browser/chromium/history" 
-upload "Browser history-journal" "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/History-journal" "$drive/browser/chromium/history" 
-upload "Browser Bookmarks" "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/Bookmarks" "$drive/browser/chromium/bookmarks" 
-upload "Browser Bookmark Merged" "C:/Users/Hugo/AppData/Local/BraveSoftware/Brave-Browser/User Data/Default/BookmarkMergedSurfaceOrdering" "$drive/browser/chromium/bookmarks" 
+upload "Browser history" "$appdata/Local/BraveSoftware/Brave-Browser/User Data/Default/History" "$drive/browser/chromium/history" 
+upload "Browser history-journal" "$appdata/Local/BraveSoftware/Brave-Browser/User Data/Default/History-journal" "$drive/browser/chromium/history" 
+upload "Browser Bookmarks" "$appdata/Local/BraveSoftware/Brave-Browser/User Data/Default/Bookmarks" "$drive/browser/chromium/bookmarks" 
+upload "Browser Bookmark Merged" "$appdata/Local/BraveSoftware/Brave-Browser/User Data/Default/BookmarkMergedSurfaceOrdering" "$drive/browser/chromium/bookmarks" 
 upload "Onedrive" "D:/OneDrive/" "$drive/onedrive" 
 
+Stop-Transcript
 # rclone tree "$drive" -all
